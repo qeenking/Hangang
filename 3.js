61279113c695178aa8cf01ebe363238dfefd9d46
qeenking <76897788+qeenking@users.noreply.github.com>
2021-04-22 22:29:47 +0900

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,33 @@
+const nextBtn = document.getElementById("next");
+const beforeBtn = document.getElementById("before");
+const firstSlide = document.querySelector(".slide1");
+const lastSlide = document.querySelector(".slide5");
+
+const SHOWING_VALUE = "showing";
+
+function handleNext() {
+    const currentSlide = document.querySelector(".showing");
+    const nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide) {
+        currentSlide.classList.remove(SHOWING_VALUE);
+        nextSlide.classList.add(SHOWING_VALUE);
+    } else {
+        currentSlide.classList.remove(SHOWING_VALUE);
+        firstSlide.classList.add(SHOWING_VALUE);
+    }
+}
+
+function handleBefore() {
+    const currentSlide = document.querySelector(".showing");
+    const beforeSlide = currentSlide.previousElementSibling;
+    if (beforeSlide) {
+        currentSlide.classList.remove(SHOWING_VALUE);
+        beforeSlide.classList.add(SHOWING_VALUE);
+    } else {
+        currentSlide.classList.remove(SHOWING_VALUE);
+        lastSlide.classList.add(SHOWING_VALUE);
+    }
+}
+
+nextBtn.addEventListener("click", handleNext);
+beforeBtn.addEventListener("click", handleBefore);
